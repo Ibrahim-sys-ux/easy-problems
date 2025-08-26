49a2818cc65bbd8d13df9850607dd6a5d896822a
Ibrahim Kutty <ibrahimkutty.c3.11@gmail.com>
2025-08-26 18:52:32 +0530

    Added Third Program Program

---
+++ 3.Print Name.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout<<"Enter The Name: ";
+    cin>>name;
+    cout<<"Namah Shivaya,"<<name<<"!"<<endl;
+    return 0;
+}
