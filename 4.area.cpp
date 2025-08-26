a2564c1d04bea03fa3efd5432da27a4496bb29f0
Ibrahim Kutty <ibrahimkutty.c3.11@gmail.com>
2025-08-26 18:58:15 +0530

    Added Fourth Program Program

---
+++ 4.area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int len,wid,area;
+    cout<<"Emter Length: ";
+    cin>>len;
+    cout<<"Enter Breath: ";
+    cin>>wid;
+    area=len*wid;
+    cout<<"Area: "<<area<<endl;
+    return 0;
+}
