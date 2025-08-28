8906313f74f18e6b00cbbc7e3777289467a8e0f0
Ibrahim Kutty <ibrahimkutty.c3.11@gmail.com>
2025-08-28 17:39:37 +0530

    Added 17 Program

---
+++ 17.circumferenceof circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float radius,circumference;
+    cout<<"Enter radius:";
+    cin>>radius;
+    circumference=2*3.14159*radius;
+    cout<<"Area of circle="<<circumference;
+    return 0;
+}
+
