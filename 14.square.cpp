4b855446f0f23af4de84f33a965de40ac651279b
Ibrahim Kutty <ibrahimkutty.c3.11@gmail.com>
2025-08-28 17:31:45 +0530

    Added 14 Program

---
+++ 14.square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,sqr;
+    cout<<"Enter a numbers:\n";
+    cin>>a;
+    sqr=a*a;
+    cout<<"Square of "<<a<<" is "<<sqr ;
+    return 0;
+}
+
+
