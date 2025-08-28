0e6bdbab8d3a2c37f8e36469f291452a8d667661
Ibrahim Kutty <ibrahimkutty.c3.11@gmail.com>
2025-08-28 17:33:48 +0530

    Added 15 Program

---
+++ 15.cube.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,cube;
+    cout<<"Enter a numbers:\n";
+    cin>>a;
+    cube=a*a*a;
+    cout<<"Cube of "<<a<<" is "<<cube ;
+    return 0;
+}
+
+
