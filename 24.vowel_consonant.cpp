9d047e6222c1a22aee6093e6cbeea1cc8afecc58
Ibrahim Kutty <ibrahimkutty.c3.11@gmail.com>
2025-08-29 18:38:49 +0530

    Added 24 Program

---
+++ 24.vowel_consonant.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter the Character: ";
+    cin>>ch;
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+       ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        cout<<ch<<" is a vowel";
+    else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+        cout<<ch<<" is a consonant";
+    else
+        cout<<ch<<" is not a alphabet";
+    return 0;
+}
