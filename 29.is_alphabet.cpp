6b071be174923433857e3b0d5644cf5d495e6a2d
Ibrahim Kutty <ibrahimkutty.c3.11@gmail.com>
2025-08-29 19:03:17 +0530

    Added 29 Program

---
+++ 29.is_alphabet.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter the Character: ";
+    cin>>ch;
+    if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+        cout<<ch<<" is a alphabet";
+    else
+        cout<<ch<<" is not a alphabet";
+    return 0;
+}
