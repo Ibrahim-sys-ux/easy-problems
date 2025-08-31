edc57e00a981260687e13f91baafd5ab73d970df
Ibrahim Kutty <ibrahimkutty.c3.11@gmail.com>
2025-08-31 19:17:50 +0530

    Added 46 Program

---
+++ 46.reverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,rev=0,temp;
+    cout<<"Enter the number:\n";
+    cin>>n;
+    temp=n;
+    while(temp>0){
+        rev=rev*10+temp%10;
+        temp/=10;
+    }
+    cout<<"Reverse:"<<rev;
+    return 0;
+}
