38cfd086c00a43bedf668c13dd8fed743f95e81e
Ibrahim Kutty <ibrahimkutty.c3.11@gmail.com>
2025-08-31 19:10:55 +0530

    Added 44 Program

---
+++ 44.Fibonacci.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num,a=0,b=1,next;
+    cout<<"Enter the Number:\n";
+    cin>>num;
+    for(int i=1;i<=num;i++){
+        cout<<a<<" ";
+        next=a+b;
+        a=b;
+        b=next;
+    }
+    return 0;
+}
